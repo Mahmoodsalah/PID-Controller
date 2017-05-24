37a2bb6a1106ecd8ae42d8d909e434600aec495a
Mahmood Salah <mahmoodsalah89@gmail.com>
2017-05-24 19:49:22 +0300

    Upload local commits

--- src/PID.h
+++ src/PID.h
@@ -2,45 +2,53 @@
 #define PID_H
 
 class PID {
+
 public:
-  /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
+    /*
+    * Errors
+    */
+    double int_cte;
+    double pre_cte;
+    double diff_cte;
+    double pre_sp;
 
-  /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
+    /*
+    * Coefficients
+    */
+    double Kp;
+    double Ki;
+    double Kd;
 
-  /*
-  * Constructor
-  */
-  PID();
+    /*
+    * Constructor
+    */
+    PID();
 
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
+    /*
+    * Destructor.
+    */
+    virtual ~PID();
 
-  /*
-  * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd);
+    /*
+    * Initialize PID.
+    */
+    void Init(double Kp, double Ki, double Kd);
 
-  /*
-  * Update the PID error variables given cross track error.
-  */
-  void UpdateError(double cte);
+    /*
+    * Update the PID error variables given cross track error.
+    */
+    void UpdateError(double cte);
 
-  /*
-  * Calculate the total PID error.
-  */
-  double TotalError();
+    /*
+    * Update the speed error variables given target speed.
+    */
+    void UpdateSpeed(double speed);
+
+    /*
+    * Calculate the total PID error.
+    */
+    double TotalError();
+
 };
 
 #endif /* PID_H */
